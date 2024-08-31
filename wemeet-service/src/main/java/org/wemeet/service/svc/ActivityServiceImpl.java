30e048267c9312f3b6d5b988401c2cd52d2d73ed
ming.jiang <sendon1982@gmail.com>
2024-08-31 19:50:10 +1000

    add table design to start to project running

--- wemeet-service/src/main/java/org/wemeet/service/svc/ActivityServiceImpl.java
+++ wemeet-service/src/main/java/org/wemeet/service/svc/ActivityServiceImpl.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.wemeet.service.gen.jpa.ActivityEntity;
+import org.wemeet.service.gen.model.ActivityResponse;
 import org.wemeet.service.repository.jpa.ActivityRepository;
 
 import java.time.LocalDateTime;
@@ -16,22 +17,26 @@ public class ActivityServiceImpl implements ActivityService {
 
     private final ActivityRepository activityRepository;
 
-    public void createNewActivity() {
+    public ActivityResponse createNewActivity(org.wemeet.service.gen.model.CreatActivityRequest request) {
 
         ActivityEntity activityEntity = new ActivityEntity();
 
-        //activityEntity.setHostUserId();
-//        activityEntity.setPostedUserId();
-//        activityEntity.setActivityName();
-//        activityEntity.setActivityStatus();
-//        activityEntity.setDescription();
-//        activityEntity.setEndDateTime();
-//        activityEntity.setPlaySpaceId();
-//        activityEntity.setMaxSlot();
-//        activityEntity.setAvailableSlot();
+        activityEntity.setHostUserId(request.getHostUserId());
+        activityEntity.setPostedUserId(request.getPostedUserId());
+        activityEntity.setActivityName(request.getActivityName());
+        activityEntity.setActivityStatus(ActivityResponse.ActivityStatusEnum.ACTIVE.name());
+        activityEntity.setDescription(request.getDescription());
+        //activityEntity.setStartDateTime(request.getStartDateTime());
+        activityEntity.setEndDateTime(request.getEndDateTime());
+        activityEntity.setPlaySpaceId(request.getPlaySpaceId());
+        activityEntity.setMaxSlot(request.getMaxSlot());
+        activityEntity.setAvailableSlot(request.getAvailableSlot());
         activityEntity.setCreatedAt(LocalDateTime.now());
         activityEntity.setCreatedBy("");
 
-        activityRepository.save(activityEntity);
+        ActivityEntity entity = activityRepository.save(activityEntity);
+
+        ActivityResponse response = new ActivityResponse();
+        return response;
     }
 }
